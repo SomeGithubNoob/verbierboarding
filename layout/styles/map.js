503855f8ff8594d05aa9312c7ef259d11bca2093
SomeGithubNoob <dubbajosh123@gmail.com>
2021-05-28 18:36:02 +1000

    updates

--- layout/styles/map.js
+++ layout/styles/map.js
@@ -1,11 +1,12 @@
-function initMap() {
-    const verbier = { lat: 46.0961, lng: 7.2286 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: verbier,
-    });
-    const marker = new google.maps.Marker({
-      position: verbier,
-      map: map,
-    });
-  }
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([7.2286, 46.0961]),
+    zoom: 14
+  })
+});
